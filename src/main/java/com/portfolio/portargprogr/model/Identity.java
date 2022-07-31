2b5c5c6478051e85e9386b06f479f9d840d55eef
Darmstedgio <darmstedgio@gmail.com>
2022-07-31 07:54:30 -0300

    saving each model, each repository, etc. its is because an error

---
+++ src/main/java/com/portfolio/portargprogr/model/Identity.java
@@ -0,0 +1,34 @@
+package com.portfolio.portargprogr.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+public class Identity {
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  private Long id;
+
+  private String name;
+  private String surname;
+  private String birthday;
+  private String city;
+  private String email;
+  private String phone;
+  private String image_path;
+  private String updated_at;
+  private String created_at;
+  private Long user_id;
+}
